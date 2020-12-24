22944f507aa12573fe5f0ae97b939bf8fd646933
modernsilpi <modernsilpi@gmail.com>
2020-12-24 21:25:38 +0530

    promocode added

--- scripts/payment.js
+++ scripts/payment.js
@@ -7,10 +7,15 @@ var productdetals=[];
 var pickupdate;
 var returndate;
 var orderid;
+var donepayment=0;
 checkout=document.querySelector('#checkoutbtn')
 checkout.addEventListener('click',(e)=>{
      e.preventDefault();
     //  decreasecart();
+    //check weather promo added or not
+    if(promocodestatus==0){
+        promocartprice=cartprice;
+    }
 pickupdate=document.getElementById('pickupdate').value;
 returndate=document.getElementById('returndate').value;
 db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').onSnapshot(snap=>{
@@ -18,7 +23,7 @@ db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').o
         productdetals.push(nap.data().name,nap.data().qty,nap.data().price,nap.data().link)
     })
 })
-    alert(`price is ${cartprice}`);
+    alert(`price is ${promocartprice}`);
     // console.log(productdetals)
     // console.log(pickupdate,returndate);
     
@@ -32,17 +37,17 @@ db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').o
     
 })
 function makeorder(){
-console.log(buyername);
-console.log(buyerphone);
-console.log(buyerlocation);
-console.log(pickupdate,returndate)
-console.log("product details",productdetals)
-console.log("total price",cartprice)
+// console.log(buyername);
+// console.log(buyerphone);
+// console.log(buyerlocation);
+// console.log(pickupdate,returndate)
+// console.log("product details",productdetals)
+// console.log("total price",promocartprice)
 // orderid=db.collection('orders').doc();
 db.collection('orders').doc(orderid.id).set({
     orderid:orderid.id,
     products:productdetals,
-    totalprice:cartprice,
+    totalprice:promocartprice,
     pickupdate:pickupdate,
     returndate:returndate,
     buyername:buyername,
@@ -60,7 +65,7 @@ function userorder(){
     db.collection('users').doc(firebase.auth().currentUser.uid).collection('orders').doc(orderid.id).set({
         orderid:orderid.id,
         products:productdetals,
-        totalprice:cartprice,
+        totalprice:promocartprice,
         pickupdate:pickupdate,
         returndate:returndate,
         buyername:buyername,
@@ -76,19 +81,26 @@ function paymentprocess(){
     // alert(orderid.id)
     var options = {
         "key": "rzp_test_EvIPsYYYs0Fsb3", // Enter the Key ID generated from the Dashboard
-        "amount": `${cartprice*100}`, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        "amount": `${promocartprice*100}`, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
         "currency": "INR",
         "name": "anoop cameras",
         "description": "best cameras in vizag",
         
         //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
         "handler": function (response){
-            alert(response.razorpay_payment_id);
+           // alert(response.razorpay_payment_id);
             // alert(response.razorpay_order_id);
             // alert(response.razorpay_signature)
+            promocodeineligible()
+           // decreasecart();
             makeorder();
             userorder();
             savetodb(response)
+            donepayment=1;
+            updatepaymentstatus(response);
+            
+           
+
         },
         "prefill": {
             "name": `${buyername}`,
@@ -105,20 +117,26 @@ function paymentprocess(){
             "color": "#3399cc"
         }
     };
-    var propay= new Razorpay(options);
-    propay.on('payment.failed', function (response){
-        // alert("payment failed");
-        makeorderfail(response);
-    //    userorderfail();
-       // savetodbfail(response);
-        // alert(response.error.code);
-        // alert(response.error.description);
-        // alert(response.error.source);
-        // alert(response.error.step);
-        // alert(response.error.reason);
-        // alert(response.error.metadata.order_id);
-        // alert(response.error.metadata.payment_id);
-});
+    var propay= new Razorpay(options,(error,order)=>{
+        if(error){
+            console.log(error);
+        } else {
+            console.log(order);
+        }
+    });
+//     propay.on('payment.failed', function (response){
+//         // alert("payment failed");
+//         makeorderfail(response);
+//     //    userorderfail();
+//        // savetodbfail(response);
+//         // alert(response.error.code);
+//         // alert(response.error.description);
+//         // alert(response.error.source);
+//         // alert(response.error.step);
+//         // alert(response.error.reason);
+//         // alert(response.error.metadata.order_id);
+//         // alert(response.error.metadata.payment_id);
+// });
     propay.open();
 }
 
@@ -126,7 +144,7 @@ function paymentprocess(){
 function savetodb(response){
     alert("payment completed");
     console.log(response)
-    decreasecart();
+   // decreasecart();
     db.collection('users').doc(firebase.auth().currentUser.uid).collection('orders').doc(orderid.id).update({
         paymentstatus:"success",
         paymentid:response.razorpay_payment_id,
@@ -143,7 +161,7 @@ function savetodb(response){
             })
         })
     })
-    updatepaymentstatus(response);
+  //  updatepaymentstatus(response);
 }
 function updatepaymentstatus(response){
     console.log("updating payment stauts in orders ")
@@ -156,72 +174,81 @@ function updatepaymentstatus(response){
      })
      cartprice=0;
      document.querySelector('.payment-sections').style.display="none";
+    // promocodeineligible();
+    decreasecart();
+}
+function promocodeineligible(){
+    if(promocodestatus==1){
+    db.collection('users').doc(firebase.auth().currentUser.uid).collection('profile').doc(firebase.auth().currentUser.uid).update({
+        promocode:"ineligible"
+    })
+}
 }
 
 
 
 //update failed payment status in user db and order db
-function savetodbfail(response){
-   // alert("payment failed");
-    console.log("payment failled");
-    db.collection('users').doc(firebase.auth().currentUser.uid).collection('orders').doc(orderid.id).update({
-        paymentstatus:"failed",
-        paymentid:response.razorpay_payment_id,
-        paymenterror: response.error.code,
-        errordescription: response.error.description,
-        reasonoffail:response.error.reason ,
-            // orderid:orderid.id,
-            // products:productdetals,
-            // totalprice:cartprice,
-            // pickupdate:pickupdate,
-            // returndate:returndate,
-            // buyername:buyername,
-            // buyerphone:buyerphone,
-            // buyerlocation:buyerlocation,
+// function savetodbfail(response){
+//    // alert("payment failed");
+//     console.log("payment failled");
+//     db.collection('users').doc(firebase.auth().currentUser.uid).collection('orders').doc(orderid.id).update({
+//         paymentstatus:"failed",
+//         paymentid:response.razorpay_payment_id,
+//         paymenterror: response.error.code,
+//         errordescription: response.error.description,
+//         reasonoffail:response.error.reason ,
+//             // orderid:orderid.id,
+//             // products:productdetals,
+//             // totalprice:cartprice,
+//             // pickupdate:pickupdate,
+//             // returndate:returndate,
+//             // buyername:buyername,
+//             // buyerphone:buyerphone,
+//             // buyerlocation:buyerlocation,
 
 
-        // paymentorderid:response.razorpay_order_id,
-        // paymentsignature:response.razorpay_signature,
-        paymentAt:firebase.firestore.FieldValue.serverTimestamp()
+//         // paymentorderid:response.razorpay_order_id,
+//         // paymentsignature:response.razorpay_signature,
+//         paymentAt:firebase.firestore.FieldValue.serverTimestamp()
         
-    }).then(()=>{
-        console.log("updating user cart")
-        return db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').onSnapshot(snap=>{
-            snap.docs.forEach(nap=>{
-                console.log(nap.id)
-                let idd=nap.id;
-                db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').doc(idd).delete();
-            })
-        })
-    }).catch(err =>{ console.log(err)
+//     }).then(()=>{
+//         console.log("updating user cart")
+//         return db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').onSnapshot(snap=>{
+//             snap.docs.forEach(nap=>{
+//                 console.log(nap.id)
+//                 let idd=nap.id;
+//                 db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').doc(idd).delete();
+//             })
+//         })
+//     }).catch(err =>{ console.log(err)
        
-    })
-    updatepaymentstatusfail(response);
-}
-function updatepaymentstatusfail(response){
-    console.log("updating payment stauts in orders ")
-     db.collection('orders').doc(orderid.id).update({
-        paymentstatus:"failed",
-        paymentid:response.razorpay_payment_id,
-        paymenterror: response.error.code,
-        errordescription: response.error.description,
-        reasonoffail:response.error.reason ,
+//     })
+//     updatepaymentstatusfail(response);
+// }
+// function updatepaymentstatusfail(response){
+//     console.log("updating payment stauts in orders ")
+//      db.collection('orders').doc(orderid.id).update({
+//         paymentstatus:"failed",
+//         paymentid:response.razorpay_payment_id,
+//         paymenterror: response.error.code,
+//         errordescription: response.error.description,
+//         reasonoffail:response.error.reason ,
         
-        // orderid:orderid.id,
-        // products:productdetals,
-        // totalprice:cartprice,
-        // pickupdate:pickupdate,
-        // returndate:returndate,
-        // buyername:buyername,
-        // buyerphone:buyerphone,
-        // buyerlocation:buyerlocation,
-        // paymentorderid:response.razorpay_order_id,
-        // paymentsignature:response.razorpay_signature,
-        paymentAt:firebase.firestore.FieldValue.serverTimestamp()
-     })
-     cartprice=0;
-     document.querySelector('.payment-sections').style.display="none";
-}
+//         // orderid:orderid.id,
+//         // products:productdetals,
+//         // totalprice:cartprice,
+//         // pickupdate:pickupdate,
+//         // returndate:returndate,
+//         // buyername:buyername,
+//         // buyerphone:buyerphone,
+//         // buyerlocation:buyerlocation,
+//         // paymentorderid:response.razorpay_order_id,
+//         // paymentsignature:response.razorpay_signature,
+//         paymentAt:firebase.firestore.FieldValue.serverTimestamp()
+//      })
+//      cartprice=0;
+//      document.querySelector('.payment-sections').style.display="none";
+// }
 
 function makeorderfail(response){
     console.log(buyername);
@@ -229,12 +256,12 @@ function makeorderfail(response){
     console.log(buyerlocation);
     console.log(pickupdate,returndate)
     console.log("product details",productdetals)
-    console.log("total price",cartprice)
+    console.log("total price",promocartprice)
     // orderid=db.collection('orders').doc();
     db.collection('orders').doc(orderid.id).set({
         orderid:orderid.id,
         products:productdetals,
-        totalprice:cartprice,
+        totalprice:promocartprice,
         pickupdate:pickupdate,
         returndate:returndate,
         buyername:buyername,
@@ -260,7 +287,7 @@ function makeorderfail(response){
         db.collection('users').doc(firebase.auth().currentUser.uid).collection('orders').doc(orderid.id).set({
             orderid:orderid.id,
             products:productdetals,
-            totalprice:cartprice,
+            totalprice:promocartprice,
             pickupdate:pickupdate,
             returndate:returndate,
             buyername:buyername,
@@ -272,17 +299,6 @@ function makeorderfail(response){
             paymenterror: `${response.error.code}`,
             errordescription: `${response.error.description}`,
             reasonoffail:`${response.error.reason}` ,
-        }).then(()=>{
-            console.log("updating user cart")
-            return db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').onSnapshot(snap=>{
-                snap.docs.forEach(nap=>{
-                    console.log(nap.id)
-                    let idd=nap.id;
-                    db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').doc(idd).delete();
-                })
-            })
-        }).catch(err =>{ console.log(err)
-           
         })
         document.querySelector('.payment-sections').style.display="none";
         //paymentprocess();
@@ -291,13 +307,169 @@ function makeorderfail(response){
 
 
 
-// decrease cart
+// decrease cart in the website 
 function decreasecart(){
+if(donepayment==1){
+    console.log("donepayment")
     db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').onSnapshot(snap=>{
         snap.docs.forEach(nap=>{
             console.log(nap.id)
-            console.log(nap.data().qty)
-            minuscart(nap.id,nap.data().qty)
+            let productid=nap.id;
+            let qte=nap.data().qty;
+            // console.log(nap.data().qty)
+            // minuscart(nap.id,nap.data().qty)
+            db.collection('categorybutton').onSnapshot(id=>{
+                id.docs.forEach(id2=>{
+                  for (var name of Object.keys(id2.data())) {
+                 //   count=count+1;
+                    console.log("object keys",Object.keys(id2.data()).length)
+                    console.log("path",id2.id,name)
+                    db.collection('categorybutton').doc(id2.id).collection(name).onSnapshot(snapp=>{
+                     snapp.docs.forEach(pan=>{
+                       if(pan.id==productid){
+
+                        console.log(`product id ${productid},searchid ${pan.id}, qty is ${qte} category${name}`);
+                        db.collection('categorybutton').doc(id2.id).collection(name).doc(productid).onSnapshot(snap=>{
+                          console.log(snap.data().qty)
+                          qte=snap.data().qty-qte;
+                          console.log(qte)
+                        })
+                        db.collection('categorybutton').doc(id2.id).collection(name).doc(productid).update({
+                          //  qty:firebase.firestore.FieldValue.increment(`-${qte}`)
+                          qty:qte
+                          
+                        })
+                       }
+                     })
+                
+                    });
+                }
+                })
+              })
         })
+    }).then(()=>{
+        console.log("updating user cart")
+        return db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').onSnapshot(snap=>{
+            snap.docs.forEach(nap=>{
+                console.log(nap.id)
+                let idd=nap.id;
+                db.collection('users').doc(firebase.auth().currentUser.uid).collection('cart').doc(idd).delete();
+            })
+        })
+    }).catch(err =>{ console.log(err)
+       
     })
-}    
+       donepayment=0; 
+  }    
+}
+
+
+//promo code function
+const promobtn=document.querySelector('#promobtn');
+var promocartprice=0;
+var promocodestatus=0;
+ document.querySelector('.promoerror').style.display="none"
+promobtn.addEventListener('click',(e)=>{
+   const promocode=document.querySelector('.promocodefield').value
+   const promoerror=document.querySelector('.promoerror')
+    console.log(promocode,"clicked");
+    document.querySelector('.promoerror').style.display="block"
+    setTimeout(function(){
+    document.querySelector('.promoerror').style.display="none";
+    }, 5000);
+    
+    db.collection('users').doc(firebase.auth().currentUser.uid).collection('profile').doc(firebase.auth().currentUser.uid).get().then(snap=>{
+        // console.log(snap.data().promocode)
+        if(snap.data().promocode=="eligible"){
+
+            db.collection('promocodes').get().then(nap=>{
+                nap.forEach(cap=>{
+                   if(promocode==cap.data().promocode){
+                       console.log("promocode added",cap.data().status)
+                    if(cap.data().status=="active"){ 
+
+                        if(cartprice >= cap.data().cutoff){
+                        
+                        if(cap.data().type=="percentage"){
+                          let offer=(cap.data().off)/100;
+                          promocartprice=Math.round(cartprice-(offer*cartprice));
+                          console.log("offer price is ",promocartprice)
+                          promocodestatus=1;
+                          var li22;
+                          li22=`
+                          <p class="text-center totalCart"><b>Totalcart:</b> ${promocartprice}</p>
+                          `;
+                          carttotal.innerHTML=li22;
+                          promoerror.innerHTML="Promo added successfully"
+            
+                        }
+                        else if(cap.data().type=="price"){
+            
+                            promocartprice=Math.round(cartprice-cap.data().off);
+                            console.log("offer price is ",promocartprice)
+                            promocodestatus=1;
+                            var li22;
+                            li22=`
+                            <p class="text-center totalCart"><b>Totalcart:</b> ${promocartprice}</p>
+                            `;
+                            carttotal.innerHTML=li22;
+                            promoerror.innerHTML="Promo added successfully"
+                            
+                        }
+                    }
+                    else { 
+                        console.log("price must be more then",cap.data().cutoff)
+                        promoerror.innerHTML=`cartprice morethen ${cap.data().cutoff}`
+                    
+                    }
+                   
+                    }
+                    else{
+                        console.log("expired")
+                        promoerror.innerHTML="Expired"
+                    }
+                   }
+                   else{
+                       console.log("not valid")
+                   }
+                })
+                })
+
+        }
+        else{
+            console.log("not eligible for this")
+            promoerror.innerHTML="Not eligible for this promo"
+        }
+    })
+    // db.collection('promocodes').get().then(nap=>{
+    // nap.forEach(cap=>{
+    //    if(promocode==cap.data().promocode){
+    //        console.log("promocode added",cap.data().status)
+    //     if(cap.data().status=="active"){ 
+    //         console.log("eligle for promo")
+    //         if(cap.data().type=="percentage"){
+    //           let offer=(cap.data().off)/100;
+    //           promocartprice=cartprice-(offer*cartprice);
+    //           console.log("offer price is ",promocartprice)
+    //           promocodestatus=1;
+
+    //         }
+    //         else if(cap.data().type=="price"){
+
+    //             promocartprice=cartprice-cap.data().off
+    //             console.log("offer price is ",promocartprice)
+    //             promocodestatus=1;
+                
+    //         }
+    //     }
+    //     else{
+    //         console.log("expired")
+    //     }
+    //    }
+    //    else{
+    //        console.log("not valid")
+    //    }
+    // })
+    // })
+})
+
